0d8d17228eae0dd5f602c58750170ee3c6f3fc9b
BASHIR <alibalti439@gmail.com>
2022-09-17 11:52:19 +0500

    4/14/2022

--- app/src/main/java/app/com/photoeditor/MainActivity.java
+++ app/src/main/java/app/com/photoeditor/MainActivity.java
@@ -18,7 +18,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,AddTextActivity.class));
             }
         });
+        findViewById(R.id.image_view1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,AddTextActivity.class));
+            }
+        });
         findViewById(R.id.textView20).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,PdfConverterActivity.class));
+            }
+        });
+        findViewById(R.id.image_view20).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,PdfConverterActivity.class));
@@ -30,7 +42,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,PngToJpgConverterActivity.class));
             }
         });
+        findViewById(R.id.image_view21).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,PngToJpgConverterActivity.class));
+            }
+        });
         findViewById(R.id.textView22).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,CompressActivity.class));
+            }
+        });
+        findViewById(R.id.interestsCard).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,CompressActivity.class));
